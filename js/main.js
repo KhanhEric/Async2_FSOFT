93c5bbf879340522a1901d188370038da616bd47
KhanhEric <khanhpham141199@gmail.com>
2023-04-18 17:31:40 +0700

    First

---
+++ js/main.js
@@ -0,0 +1,38 @@
+(function () {
+  var app = angular.module("myApp", []);
+
+  app.controller("demoCtrl", ["$scope", DemoController]);
+
+  // // Problem 1: Callbacks
+  function DemoController($scope) {
+    //    TODO: Code here!
+    let urlUserApi = "https://jsonplaceholder.typicode.com/users";
+    let urlPostApi = "https://jsonplaceholder.typicode.com/posts?userId=";
+    $.get(urlUserApi, function (users) {
+      users.forEach((user) => {
+        $.get(urlPostApi + user.id, function (postsData) {
+          user.posts = postsData;
+          $.get(user.id);
+          $scope.$apply(function () {
+            $scope.users = users;
+          });
+        });
+      });
+      try {
+      } catch (error) { }
+    });
+  }
+
+  // Problem 2: Promise
+  // function DemoController($scope) {
+  //    TODO: Code here!
+
+  // }
+
+  // Problem 03: Generators // BO QUA
+
+  // Problem 04: async/await
+  // async function DemoController($scope) {
+  //   TODO: Code here!
+  // }
+})();
