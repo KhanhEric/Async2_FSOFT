930083725e176a1c3f05ba20f26be5ac01cadfc6
KhanhEric <khanhpham141199@gmail.com>
2023-04-19 16:31:17 +0700

    Problem 2

--- js/main.js
+++ js/main.js
@@ -35,26 +35,44 @@
     let userApi = "https://jsonplaceholder.typicode.com/users";
     let postApi = "https://jsonplaceholder.typicode.com/posts?userId=";
     let commentApi = "https://jsonplaceholder.typicode.com/comments?postId=";
-    let users = [];
+    let userPromise = [];
     function getData(url) {
       return new Promise((resolve, reject) => {
-        $.get(url, function (data, status) {
-          if (status == "success") {
-            resolve(data);
-          }
-          reject(new Error(`Bao loi tai : ${url}`));
+        $.get(url, function (data) {
+          resolve(data);
         });
       });
     }
     getData(userApi)
-      .then((data) => {
-        data.forEach((user) => {
-          users = data;
+    .then((data) => {
+      data.forEach((user) => {
+        users = data;
+        userPromise.push(getData(postApi + user.id));
+      });
+      return Promise.all(userPromise);
+    })
+    .then((postData) => {
+      let postPromise = [];
+      users.forEach((user, index) => {
+        user.posts = postData[index];
+
+        user.posts.forEach((post) => {
+          postPromise.push(getData(commentApi + post.id));
         });
-        $scope.$apply(function () {
-          $scope.users = users;
+      });
+
+      return Promise.all(postPromise);
+    })
+    .then((commentData) => {
+      users.forEach((user) => {
+        user.posts.forEach((post) => {
+          post.comments = commentData[post.id - 1];
         });
-      })
+      });
+      $scope.$apply(function () {
+        $scope.users = users;
+      });
+    })
       .catch((err) => {
         console.log(err.message);
       });
