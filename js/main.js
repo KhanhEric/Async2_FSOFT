33bdf804a42a5833f32974441129cec7594ea410
KhanhEric <khanhpham141199@gmail.com>
2023-04-20 14:11:51 +0700

    problem 4

--- js/main.js
+++ js/main.js
@@ -30,58 +30,78 @@
   // }
 
   // Problem 2: Promise
-  function DemoController($scope) {
-    //    TODO: Code here!
-    let userApi = "https://jsonplaceholder.typicode.com/users";
-    let postApi = "https://jsonplaceholder.typicode.com/posts?userId=";
-    let commentApi = "https://jsonplaceholder.typicode.com/comments?postId=";
-    let userPromise = [];
-    function getData(url) {
-      return new Promise((resolve, reject) => {
-        $.get(url, function (data) {
-          resolve(data);
-        });
-      });
-    }
-    getData(userApi)
-    .then((data) => {
-      data.forEach((user) => {
-        users = data;
-        userPromise.push(getData(postApi + user.id));
-      });
-      return Promise.all(userPromise);
-    })
-    .then((postData) => {
-      let postPromise = [];
-      users.forEach((user, index) => {
-        user.posts = postData[index];
-
-        user.posts.forEach((post) => {
-          postPromise.push(getData(commentApi + post.id));
-        });
-      });
-
-      return Promise.all(postPromise);
-    })
-    .then((commentData) => {
-      users.forEach((user) => {
-        user.posts.forEach((post) => {
-          post.comments = commentData[post.id - 1];
-        });
-      });
-      $scope.$apply(function () {
-        $scope.users = users;
-      });
-    })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }
+  // function DemoController($scope) {
+  //   //    TODO: Code here!
+  //   let userApi = "https://jsonplaceholder.typicode.com/users";
+  //   let postApi = "https://jsonplaceholder.typicode.com/posts?userId=";
+  //   let commentApi = "https://jsonplaceholder.typicode.com/comments?postId=";
+  //   let userPromise = [];
+  //   let postPromise = [];
+  //   function getData(url) {
+  //     return new Promise((resolve, reject) => {
+  //       $.get(url, function (data) {
+  //         resolve(data);
+  //       });
+  //     });
+  //   }
+  //   getData(userApi)
+  //     .then((data) => {
+  //       data.forEach((user) => {
+  //         users = data;
+  //         userPromise.push(getData(postApi + user.id));
+  //       });
+  //       return Promise.all(userPromise);
+  //     })
+  //     .then((postData) => {
+  //       users.forEach((user, index) => {
+  //         user.posts = postData[index];
+  //         user.posts.forEach((post) => {
+  //           postPromise.push(getData(commentApi + post.id));
+  //         });
+  //         $scope.$apply(function () {
+  //           $scope.users = users;
+  //         });
+  //       });
+  //       return Promise.all(postPromise);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //     });
+  // }
 
   // Problem 03: Generators // BO QUA
 
   // Problem 04: async/await
-  // async function DemoController($scope) {
-  //   TODO: Code here!
-  // }
+  async function DemoController($scope) {
+    //   TODO: Code here!
+    let userApi = "https://jsonplaceholder.typicode.com/users";
+    let postApi = "https://jsonplaceholder.typicode.com/posts?userId=";
+    let commentApi = "https://jsonplaceholder.typicode.com/comments?postId=";
+    let users = [];
+    function getData(url) {
+      return new Promise((resolve, reject) => {
+        $.get(url, function (data) {
+            resolve(data);
+        });
+      });
+    }
+
+    (async function getDataFromAsync() {
+      try {
+        var userData = await getData(userApi);
+      } catch (err) {}
+      users = userData;
+      for (let user of userData) {
+        let postData = await getData(postApi + user.id);
+        user.posts = postData;
+        for (let post of postData) {
+          let commentData = await getData(commentApi + post.id);
+          post.comments = commentData;
+        }
+        $scope.$apply(function () {
+          $scope.users = users;
+        });
+      }
+    })();
+  }
 })();
